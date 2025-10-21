5bdc6e2111f6ca9cbfc2cdcb9dfcfa4d5f84b667
m1sha <mishapavelyev141@mail.ru>
2025-10-21 21:42:45 +0300

    initial

---
+++ src/main/kotlin/com/mlone23/minibank/MinibankApplication.kt
@@ -0,0 +1,11 @@
+package com.mlone23.minibank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinibankApplication
+
+fun main(args: Array<String>) {
+	runApplication<MinibankApplication>(*args)
+}
---
+++ src/test/kotlin/com/mlone23/minibank/MinibankApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mlone23.minibank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinibankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
