ed4b0eb050eaac380088a627da40c54c3ede5cb0
m1sha <mishapavelyev141@mail.ru>
2025-10-22 20:40:51 +0300

    initial

--- src/main/kotlin/com/mlone23/minibank/controller/RegistryController.kt
+++ src/main/kotlin/com/mlone23/minibank/controller/RegistryController.kt
@@ -1,4 +1,20 @@
 package com.mlone23.minibank.controller
 
-class RegistryController {
+import com.mlone23.minibank.model.User
+import com.mlone23.minibank.repository.UserRepository
+import com.mlone23.minibank.service.RegisterService
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+
+class RegistryController(val user: User, val registerService: RegisterService) {
+
+    fun registryController(@RequestBody email: String, @RequestBody password: String, @RequestBody name: String): User{
+        val user = registerService.registryUser(email, password, name)
+        return user
+    }
 }
