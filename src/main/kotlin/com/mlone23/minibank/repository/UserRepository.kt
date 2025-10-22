478ddf0bf97d60582ed7bf7a093a835da13eb393
skywalker <wsky3478@gmail.com>
2025-10-22 21:14:36 +0300

    add Swagger UI and Spring Security 2

--- src/main/kotlin/com/mlone23/minibank/repository/UserRepository.kt
+++ src/main/kotlin/com/mlone23/minibank/repository/UserRepository.kt
@@ -1,6 +1,7 @@
 package com.mlone23.minibank.repository
 
 import com.mlone23.minibank.model.User
+import jakarta.validation.constraints.Email
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
