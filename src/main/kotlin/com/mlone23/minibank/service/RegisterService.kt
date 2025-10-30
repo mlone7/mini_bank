845a83b1e4c66204e19f01587e33c7de4c81691b
skywalker <wsky3478@gmail.com>
2025-10-30 15:50:47 +0300

    add account

--- src/main/kotlin/com/mlone23/minibank/service/RegisterService.kt
+++ src/main/kotlin/com/mlone23/minibank/service/RegisterService.kt
@@ -1,8 +1,6 @@
 package com.mlone23.minibank.service
 
-import com.mlone23.minibank.model.Account
 import com.mlone23.minibank.model.User
-import com.mlone23.minibank.repository.AccountRepository
 import com.mlone23.minibank.repository.UserRepository
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -10,7 +8,6 @@ import org.springframework.stereotype.Service
 @Service
 class RegisterService(
     val userRepository: UserRepository,
-    val accountRepository: AccountRepository,
     val passwordEncoder: PasswordEncoder
 ) {
 
@@ -19,16 +16,6 @@ class RegisterService(
         val encodePassword = passwordEncoder.encode(password)
 
         val newUser = User(email, name, encodePassword)
-        val savedUser = userRepository.save(newUser)
-
-        val newAccount = Account(name, 0.0, savedUser)
-        accountRepository.save(newAccount)
-
-        return savedUser
+        return userRepository.save(newUser)
     }
-
-
-
-
-
 }
